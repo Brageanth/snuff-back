72507901cccac1b014e70f571e2eff9a027fb4e1
brageanth <brageanth@gmail.com>
2018-12-05 11:07:37 -0500

    cambio base de datos

--- snuff_back/settings.py
+++ snuff_back/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'snuff_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'snuff-back',
@@ -83,8 +83,13 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
