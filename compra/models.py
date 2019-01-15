fd0a038f93ad94f08e8939b975275f9418231d89
brageanth <brageanth@gmail.com>
2019-01-14 21:18:11 -0500

    Imagen Compra

--- compra/models.py
+++ compra/models.py
@@ -16,7 +16,7 @@ class Personalizada(models.Model):
     entregada = models.BooleanField()
     carrito = models.BooleanField()
     pagado = models.BooleanField()
-    imagen = models.CharField(max_length=100, primary_key=True)
+    imagen = models.CharField(max_length=100)
 
     def __str__(self):
         return self.prenda.tipo
