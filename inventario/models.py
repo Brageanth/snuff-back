2ec7cad2737bec1dd6d228f1ce1713cc609fc9f7
brageanth <brageanth@gmail.com>
2019-01-21 12:54:45 -0500

    cantidad estampado

--- inventario/models.py
+++ inventario/models.py
@@ -81,6 +81,7 @@ class Estampado(models.Model):
     color = models.ForeignKey(Colore, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
     stock = models.BooleanField()
+    cantidad = models.
     imagenPrenda = models.ImageField()
     imagenEstampado = models.ImageField()
     imagenPreview = models.ImageField()
