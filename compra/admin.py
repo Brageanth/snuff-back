8acc4718637a5727f7b06ec6b53eb6d545946922
brageanth <brageanth@gmail.com>
2019-02-01 12:08:33 -0500

    Error custom admin

--- compra/admin.py
+++ compra/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Personalizada
 
 class PersonalizadaAdmin(admin.ModelAdmin):
-    list_display = ('prenda', 'estampado', 'usuario', 'fabricada', 'entregada', 'pagada', 'precio_total')
+    list_display = ('prenda', 'estampado', 'usuario', 'fabricada', 'entregada', 'pagado', 'precio_total')
     search_fields = ('usuario')
 
 admin.site.register(Personalizada, PersonalizadaAdmin)
