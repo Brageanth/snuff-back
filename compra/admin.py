6dcbd2b19e3a92dc7900d05d8171b6d82bb4307c
brageanth <brageanth@gmail.com>
2019-02-01 12:06:59 -0500

    Custom admigf

--- compra/admin.py
+++ compra/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Personalizada
 
-admin.site.register(Personalizada)
+class PersonalizadaAdmin(admin.ModelAdmin):
+    list_display = ('prenda', 'estampado', 'usuario', 'fabricada', 'entregada', 'pagada', 'precio_total')
+    search_fields = ('usuario')
+
+admin.site.register(Personalizada, PersonalizadaAdmin)
