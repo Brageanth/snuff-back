7405cbc6bd460ff11b03311e0f2f04e791046925
David Rocha Tabares <davidrochatabares@192.168.1.103>
2019-02-02 15:11:10 -0500

    Conexion payu

--- pasarela_pagos/views.py
+++ pasarela_pagos/views.py
@@ -6,10 +6,9 @@ from rest_framework.response import Response
 import urllib3
 import json
 
-http = urllib3.PoolManager()
-
 @api_view(['GET', 'POST'])
 def consulta_bancos(request):
+    http = urllib3.PoolManager()
     if request.method == 'GET':
         data = {
             "test": True,
@@ -21,6 +20,4 @@ def consulta_bancos(request):
             }
         }
         encoded_data = json.dumps(data).encode('utf-8')
-        r = http.request('POST', 'https://sandbox.api.payulatam.com/reports-api/4.0/service.cgi', body=data, headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
-        print(r)
-        return Response(r)
+        return Response(http.request('POST', 'https://sandbox.api.payulatam.com/reports-api/4.0/service.cgi', body=encoded_data, headers={'Content-Type': 'application/json', 'Accept': 'application/json'}))
