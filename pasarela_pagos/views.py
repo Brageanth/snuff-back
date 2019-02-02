595e46187f2fddc0e703855a3d1ae59b1c002aca
David Rocha Tabares <davidrochatabares@192.168.1.104>
2019-02-01 22:02:22 -0500

    Conexion payu

--- pasarela_pagos/views.py
+++ pasarela_pagos/views.py
@@ -3,10 +3,11 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 
 
 @api_view(['GET', 'POST'])
-def consulta_bancos(requests):
+def consulta_bancos():
     if request.method == 'GET':
         r = requests.post("https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi", data={
             "test": true,
