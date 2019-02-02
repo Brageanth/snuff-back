fa641c091a5ff615c0cc0ff7cfdfa73f7dd619be
David Rocha Tabares <davidrochatabares@192.168.1.103>
2019-02-02 17:46:42 -0500

    completar modelo estampado

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Prenda, Colore, Talla, Estampado
 
+class EstampadoAdmin(admin.ModelAdmin):
+    list_display = ('prenda', 'nombre', 'categoria', 'cantidad', 'stock')
+    search_fields = ('nombre',)
+
 admin.site.register(Prenda)
 admin.site.register(Colore)
 admin.site.register(Talla)
-admin.site.register(Estampado)
+admin.site.register(Estampado, EstampadoAdmin)
